5a79da7b18925fb79988f1bfe744cf60356e4301
hhhiugh <lucasinvestimentoprogramador@gmail.com>
2026-03-31 20:30:33 -0300

    Update: Forçando atualização de lógica HTTPS e JS

--- dashboard.php
+++ dashboard.php
@@ -1,51 +1,22 @@
 <!DOCTYPE html>
-
-<?php
-
-$soma = file_get_contents('fullUser.json');
-$d = json_decode($soma,true);
-$uuid = count($d);
-
-?>
-
 <html lang="pt-br">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Dashboard</title>
-    <link rel="stylesheet" href="css/dashStyle.css">
-</head>
-
-<body>
-
-    <div class="toolbar">
-        <a class="sair" href="https://xvideos.com"> Sair </a>
-        <a class="adm" href="https://wa.me/+5554991267036">Contato</a>
-        <h1 class="title">Painel Z8HX - ADMIN</h1>
-    </div>
-
-    <div class="fundo">
-
-        <div class="drawer">
-
-            <a style="background-color: transparent ;margin: 0px; padding: 0px;" href="dashboard.php"><img style=" background-color: rgba(255, 255, 255, 0.438); border-radius: 3px;" src="img/Home100.png"></a>
-            <a style="background-color: transparent ;margin: 0px;padding: 0px;" href="create.php"><img  src="img/addUser100.png"></a>
-            <a href="view.php" style="background-color: transparent ; margin: 0px; padding: 0px;"><img src="img/ViewUser100.png"></a>
-            <a href="remove.php" style="background-color: transparent ; margin: 0px; padding: 0px;"><img src="img/RemovUser100.png"></a>
-
-        </div>
-
-        <div class="users">
-            <img class="usersImg" src="img/ViewUser100.png" alt="">
-            <small><?php echo $uuid ?></small>
-        </div>
-
-    </div>
-
-
-
+<head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Dashboard Z8HX</title><link rel="stylesheet" href="css/dashStyle.css"></head>
+<body style="display:none;" id="content">
+<div class="toolbar"><a class="sair" href="index.html"> Sair </a><h1 class="title">Painel Z8HX - ADMIN</h1></div>
+<div class="users"><img src="img/ViewUser100.png" width="50"><small id="user-count">...</small></div>
+<script>
+if (location.protocol !== 'https:' && location.hostname !== 'localhost') {
+    location.replace('https:' + location.href.substring(location.protocol.length));
+} else { document.getElementById('content').style.display = 'block'; }
+async function carregar() {
+    try {
+        const res = await fetch('fullUser.json?t=' + new Date().getTime());
+        const dados = await res.json();
+        document.getElementById('user-count').innerText = Array.isArray(dados) ? dados.length : Object.keys(dados).length;
+    } catch (e) { document.getElementById('user-count').innerText = "0"; }
+}
+carregar();
+</script>
 </body>
-
 </html>
--- login.php
+++ login.php
@@ -1,71 +1,25 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<?php
-
-?>
-
-
-<?php
-    if(isset($_POST['logar'])){
-
-        $nome = $_POST['user'];
-        $senha = $_POST['pass'];
-      
-        if($nome == ""){
-
-        }else{
-            if($senha == ""){
-
-            }else{
-
-
-                if($nome = "PIROCA" && $senha = "ADMIN" ){
-
-                    $script = "<script> window.location = 'dashboard.php';</script>";
-                    echo $script;
-                    exit;
-
-                }
-      
-       
-
-            }
-        }
-
-    }
-?>
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
-
-    <link rel="stylesheet" href="css/loginStyle.css">
-
-</head>
-
+<head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Login Z8HX</title><link rel="stylesheet" href="css/loginStyle.css"></head>
 <body>
-
-    <form action="" method="post">
-
-        <h1>LOGIN Z8HX</h1>
-
-        <div class="login">
-
-            <input type="text" name="user" placeholder="Username: ">
-            <input type="text" name="pass" placeholder="Password: ">
-            <p style="padding: 0px ; margin:0px;"><a href="https://wa.me/+5554991267036">Não possui senha?</a> </p>
-            <input name="logar" class="btn" type="submit" value="Login">
-
-        </div>
-
-    </form><br>
-
-   
-  
-
-
+<form onsubmit="event.preventDefault(); validarLogin();">
+    <h1>LOGIN Z8HX</h1>
+    <div class="login">
+        <input type="text" id="user" placeholder="Username: " required>
+        <input type="password" id="pass" placeholder="Password: " required>
+        <button type="submit" class="btn">Login</button>
+    </div>
+</form>
+<script>
+function validarLogin() {
+    const u = document.getElementById('user').value;
+    const p = document.getElementById('pass').value;
+    if (u === "PIROCA" && p === "ADMIN") {
+        alert("Logado com sucesso!");
+        window.location.href = "dashboard.php";
+    } else { alert("Usuário ou senha incorretos!"); }
+}
+</script>
 </body>
-
 </html>
